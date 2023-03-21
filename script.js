7b25b519df762f9c7d966d9f4821bce073b5efcf
JulioVianaDev <JulioVianaDev@gmail.com>
2023-03-21 10:15:38 -0300

    movendo os arquivos

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-
-
-function script() {
+function sendData() {
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
   
+  const data = {
+    name: name,
+    email: email
+  };
+  
+  fetch('https://http-fetch-template-default-rtdb.firebaseio.com/pessoas.json', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
 }
