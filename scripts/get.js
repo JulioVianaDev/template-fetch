deb27b85cc67650364a89160b0cc0bc8a8b44e92
JulioVianaDev <JulioVianaDev@gmail.com>
2023-03-21 13:47:50 -0300

    refatorando

--- scripts/get.js
+++ scripts/get.js
@@ -1,7 +1,6 @@
 const url ="https://http-fetch-template-default-rtdb.firebaseio.com/pessoas.json"
 // método 1
-var allData= {}
-
+var position = 0;
 async function PegarDados(){
   const resultado = await fetch(url);
   const resultadoConvertido = await resultado.json();
@@ -10,29 +9,28 @@ async function PegarDados(){
   const arr = Object.entries(resultadoConvertido).map(([id, values]) => ({ id, ...values }));
 
   // console.log(arr);
-  allData= arr;
-  // console.log(allData)
   // console.log(array);
   // console.log(array[0].email);
   // colocar os dados em tela
   function generateTableRows(arr) {
-    console.log(arr)
-    return arr.map(item => `
+    
+    return arr.map(item => {
+      console.log(item);
+      `
       <tr>
         <td>${item.id}</td>
         <td>${item.name}</td>
         <td>${item.email}</td>
       </tr>
-    `).join('');
+    `}).join('');
   }
   
   const tableBody = document.getElementById("tbody")
-  console.log(tableBody)
+  // console.log(tableBody)
   tableBody.innerHTML = generateTableRows(arr);
 }
 PegarDados();
 // método 2 
-console.log(allData);
 async function PegarDados2(){
   const resultado2 = await fetch(url).then((res)=>res.json());
   const arr = Object.entries(resultado2).map(([id, values]) => ({ id, ...values }));
