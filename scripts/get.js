56f6685f535b2baaa9891e72603d02396dafa0bc
JulioVianaDev <JulioVianaDev@gmail.com>
2023-03-21 11:50:57 -0300

    fetch com os datas crus

--- scripts/get.js
+++ scripts/get.js
@@ -1,11 +1,9 @@
-function getJSONData(url) {
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      const jsonData = document.getElementById('json-data');
-      jsonData.innerText = JSON.stringify(data);
-    })
-    .catch(error => console.error(error));
-}
-
-getJSONData('https://http-fetch-template-default-rtdb.firebaseio.com/pessoas.json');
+// fetch("https://http-fetch-template-default-rtdb.firebaseio.com/pessoas.json")
+//   .then(res=>{
+//     return  res.json()
+//   })
+//   .then(data=>{
+//       console.log(data)
+//       // data.map(user=>console.log(user))
+//     }
+//   )
