001660a5e953dd254ad18387872c1b640607ed3b
JulioVianaDev <JulioVianaDev@gmail.com>
2023-03-21 12:08:35 -0300

    montando os get

--- scripts/get.js
+++ scripts/get.js
@@ -1,9 +1,14 @@
-// fetch("https://http-fetch-template-default-rtdb.firebaseio.com/pessoas.json")
-//   .then(res=>{
-//     return  res.json()
-//   })
-//   .then(data=>{
-//       console.log(data)
-//       // data.map(user=>console.log(user))
-//     }
-//   )
+const url ="https://http-fetch-template-default-rtdb.firebaseio.com/pessoas.json"
+// método 1
+async function PegarDados(){
+  const resultado = await fetch(url);
+  const resultadoConvertido = await resultado.json();
+  console.log(resultadoConvertido)
+}
+PegarDados();
+// método 2 
+async function PegarDados2(){
+  const resultado2 = await fetch(url).then((res)=>res.json());
+  console.log(resultado2);
+}
+PegarDados2();
