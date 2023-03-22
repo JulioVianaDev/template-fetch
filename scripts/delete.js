da02011f191f3ca40dcba138bebdebb1c76c0324
JulioVianaDev <JulioVianaDev@gmail.com>
2023-03-22 19:08:07 -0300

    explicando o porque a url é diferente

--- scripts/delete.js
+++ scripts/delete.js
@@ -1,3 +1,8 @@
+// o endpoint do banco para deletar muda, o motivo disso é o id
+// eu não posso apenas dizer pro banco "delete isso"
+// sem explicar o que é isso
+// eu preciso explicar o que ele vai deletar do banco
+// e para isso nós usamos o id
 const urlComId = 'https://http-fetch-template-default-rtdb.firebaseio.com/pessoas/'
 function deleteData(id) {
   fetch(`${urlComId}${id}.json`, {
