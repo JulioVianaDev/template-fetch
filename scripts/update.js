0f76730700b6b344d89f897a7d112881a740f6df
JulioVianaDev <JulioVianaDev@gmail.com>
2023-03-22 19:24:16 -0300

    comentando o motivo do var

--- scripts/update.js
+++ scripts/update.js
@@ -1,7 +1,14 @@
+// Diferença entre verbo HTTP 
 // PUT VS PATCH
 // PUT MODIFICA O OBJETO INTEIRO
-// PATCH MODIFICA APENAS OS ATRIBUTOS
+// PATCH MODIFICA APENAS OS ATRIBUTOS que estão sendo enviados
+// como nós estamos utilizando o mesmo formulário nós precisamos dizer
+// que não estamos mais no input mode. para fazer isso criamos uma váriavel
+// que vai checar se o texto escrito no botão é "Edite aqui!"
+// no javascript nós podemos fazer perguntas lógicas e elas
+// retornam uma booleana dizendo se a resposta é sim ou não
 var editMode = document.getElementById("submit").innerHTML !== "Edite aqui!";
+// agora nós precisamos enviar os dados atuais para o input caso eu queira editar o item
 function enviandoDadosAtuais(id,nameItem,emailItem){
   document.getElementById('email').value = emailItem;
   document.getElementById('name').value = nameItem;
