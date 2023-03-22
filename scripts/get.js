2ded69d810e2c1a85d76b4afcc5f1466d6e6d4ce
JulioVianaDev <JulioVianaDev@gmail.com>
2023-03-22 18:24:35 -0300

    comentado o get

--- scripts/get.js
+++ scripts/get.js
@@ -6,9 +6,20 @@ const url ="https://http-fetch-template-default-rtdb.firebaseio.com/pessoas.json
 async function PegarDados(){
   // primeiro de tudo fazemos uma requisição para a api e esperamos a resposta
   const resultado = await fetch(url);
+  // agora convertendo o resultado em json
   const resultadoConvertido = await resultado.json();
+  // neste exemplo eu mostro que quanto coletar o json se lermos apenas os valores
+  // que estão no objeto perdemos o id
   const array = Object.values(resultadoConvertido);
+  // porem nós podemos fazer o seguinte, para todas as entradas de objeto,
+  // pegamos o id e os valores e depois disso concatenamos no map para criar novos objetos
   const arr = Object.entries(resultadoConvertido).map(([id, values]) => ({ id, ...values }));
+  // agora o que fazemos, nós criamos uma função que recebe um array
+  // ai com este array ela vai criar os items e criar uma nova linha da tabela
+  // porem repare que ela chama as funções do delete passando o id que ela vai precisar
+  // e depois disso no enviando os dados atuais a função pede o id, name e email
+  // em casos extras precisamos passar tudo que está no banco
+  // e para finalizar damos um join para juntar
   function generateTableRows(arr) {
     return arr.map((item,index) => 
       `<tr>
@@ -21,9 +32,12 @@ async function PegarDados(){
       </tr>
     `).join('');
   }
-  
-  const tableBody = document.getElementById("tbody")
+  // aqui nós pegamos o elemento que está lá no html, que no caso é o body da tabela
+  // que contem o id "tbody"
+  const tableBody = document.getElementById("tbody");
+  // agora ao nosso table body nós juntamos o HTML com o inerHTML 
+  // chamando a função de gerar itens e depois enviando o array que criamos
   tableBody.innerHTML = generateTableRows(arr);
 }
-
+// pegando os dados quando nós iniciarmos o app
 PegarDados();
