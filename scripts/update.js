90e917df88b4ef6db85e4c5e0cbbb8ab0529d3b5
JulioVianaDev <JulioVianaDev@gmail.com>
2023-03-22 13:19:40 -0300

    editando o update

--- scripts/update.js
+++ scripts/update.js
@@ -1,7 +1,11 @@
 // PUT VS PATCH
 // PUT MODIFICA O OBJETO INTEIRO
 // PATCH MODIFICA APENAS OS ATRIBUTOS
-
+function enviandoDadosAtuais(id,item){
+  const name = document.getElementById('name');
+  const email = document.getElementById('email');
+  
+}
 function editData(id,item){
   fetch(`${urlComId}${id}.json`, {
     method: 'PATCH',
