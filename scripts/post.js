01466d28103c0c3bc115f9553afdbf91ee3caa18
JulioVianaDev <JulioVianaDev@gmail.com>
2023-03-22 18:45:50 -0300

    explicando o if do post

--- scripts/post.js
+++ scripts/post.js
@@ -1,10 +1,14 @@
 function sendData() {
+  // quando essa função é chamada eu checo se eu estou no modo edit
+  // esse modo edit só existe se eu tenho o arquivo update.js
+  // então se é a primeira aula e não estou editando pode não fazer o if
+  // porque eu estou postando não editando AINDA
   if(editMode){
     const id = document.getElementById("submit").getAttribute("data-id");
-
     editData(id);
     return
   }
+  
   const name = document.getElementById('name').value;
   const email = document.getElementById('email').value;
   
