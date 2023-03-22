585c9294618d6c190b4c1a414737952441625d82
JulioVianaDev <JulioVianaDev@gmail.com>
2023-03-22 19:11:00 -0300

    delete comentado

--- scripts/delete.js
+++ scripts/delete.js
@@ -5,6 +5,9 @@
 // e para isso nós usamos o id
 const urlComId = 'https://http-fetch-template-default-rtdb.firebaseio.com/pessoas/'
 function deleteData(id) {
+  // para fazer a url ficar estática nós precisamos concatenar o id
+  // passamos o método HTTP delete e explicamos que o formato é json
+  // não precisamos passar nenhum corpo então o delete já está funcionando
   fetch(`${urlComId}${id}.json`, {
     method: 'DELETE',
     headers: {
